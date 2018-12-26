0d665b6ac6945c9eb083d4db9ab135ae090c0c41
Makan Dey <makan@mastacoder.com>
2018-12-25 20:34:17 -0500

    Build 2

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ import time
 GPIO.setmode(GPIO.BCM) # how the pins are read
 
 # Pins in use:
+GP_MOTOR = [
+    [17, 27],
+    [22, 23]
+]
+
 GP_DISTANCE = [
     # TRIG, ECHO
     [2, 3], # front
     [4, 14], # right
     [15, 18] # left
 ]
+
+# set motor mode
+for a in GP_MOTOR:
+    GPIO.setup(a[0], GPIO.OUT)
+    GPIO.setup(a[1], GPIO.OUT)
+
+# turn them into PWM
+l1 = GPIO.PWM(17, 50)
+l2 = GPIO.PWM(27, 50)
+r1 = GPIO.PWM(22, 50)
+r2 = GPIO.PWM(23, 50)
 
 # set distance mode
 for a in GP_DISTANCE:
@@ -36,8 +52,22 @@ def distance(side): # pass in index of side
 
     return ((stop - start) * 34300) / 2
 
+# Used for wheel and direction driving.
+def drive(left, right, reverse):
+    if reverse:
+        GPIO.output(17, False)
+        GPIO.output(22, False)
+        l2.start(left)
+        r2.start(right)
+    else:
+        GPIO.output(27, False)
+        GPIO.output(23, False)
+        l1.start(left)
+        r1.start(right)
+
 try:
     while True:
+        drive(75, 75, False)
         print(round(distance(0)))
         time.sleep(1)
 
