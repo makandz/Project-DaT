1c9cda9961ac94b6e110d6a1cd2642b6b3f453e7
Makan Dey <makan@mastacoder.com>
2018-12-27 12:12:55 -0500

    Build 4
    - Bug fixes
    - Can handle turns now

--- main.py
+++ main.py
@@ -55,39 +55,51 @@ def distance(side): # pass in index of side
 # Used for wheel and direction driving.
 def drive(left, right, reverse):
     if reverse:
-        GPIO.output(17, False)
-        GPIO.output(22, False)
+        l1.start(0)
+        r1.start(0)
         l2.start(left)
         r2.start(right)
     else:
-        GPIO.output(27, False)
-        GPIO.output(23, False)
+        l2.start(0)
+        r2.start(0)
         l1.start(left)
         r1.start(right)
+    print("Left", left, "Right", right, "Reverse", reverse)
 
 # reverse and do a turn.
 def processTurn(left):
     drive(70, 70, True)
-    time.sleep(2)
+    time.sleep(0.5)
     if left:
         drive(70, 25, False)
     else:
         drive(25, 70, False)
-    time.sleep(2)
+    time.sleep(0.8)
+
+# for testing
+drive(0, 0, False)
+time.sleep(5)
 
 try:
     while True:
+        ld = distance(2)
+        rd = distance(1)
         if distance(0) < 10:
             drive(0, 0, False)
-            ld = distance(2)
-            rd = distance(1)
             if ld > rd and ld > 20:
                 processTurn(True)
             elif rd > ld and rd > 20:
                 processTurn(False)
         else:
-            drive(70, 70, False)
-        time.sleep(1)
+            if ld < 20:
+                push = (20 - ld) * 2
+                drive(35, 40 + push, False)
+            elif distance(1) < 20:
+                push = (20 - rd) * 2
+                drive(40 + push, 35, False)
+            else:
+                drive(70, 70, False)
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
     print("Measurement stopped by User")
